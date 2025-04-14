18958cf1d9927972541cd60ecd810c4714c7cc6b
aswinnadh <aswinnadh9803@gmail.com>
2025-04-14 21:43:20 +0530

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './components/Navbar'
+import NavBar from './components/NavBar';
 import Hero from './sections/Hero'
 import Projects from './sections/Projects'
 import LogoShowcase from './sections/LogoShowcase'
